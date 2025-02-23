44f9db3305a695829392f30add04bbb40d1c2b1f
Rachael <156548776+rachaelhuang@users.noreply.github.com>
2025-02-23 14:20:54 -0500

    adding footer

---
+++ components/footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <nav className="footer">
+      
+    </nav>
+  );
+};
+
+export default Footer;
